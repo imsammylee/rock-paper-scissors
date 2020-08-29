6580f84c7ad0d47737c9735be7047b1821580185
imsammylee <vsamuel.lee@gmail.com>
2020-08-29 15:35:28 +0900

    updated

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
 //generate a random variable for computer
 function computerPlay() {
     const items = ["rock", "paper", "scissors"];
     return items[Math.floor(Math.random() * 3)];  
 }
-
 
 //change player's selection image
 function imageChange() {
@@ -15,17 +13,14 @@ function imageChange() {
     const playerScissors = document.getElementById("scissors");
     playerScissors.addEventListener("click", () => { document.querySelector(".player-selection").src = "scissors_image.png" });
   }
-//change computer's selection image
 
-
-//call functions
+//call functions on click
 const playerRock = document.getElementById("rock");
 playerRock.addEventListener("click", playRound, imageChange());
 const playerPaper = document.getElementById("paper");
 playerPaper.addEventListener("click", playRound, imageChange());
 const playerScissors = document.getElementById("scissors");
 playerScissors.addEventListener("click", playRound, imageChange());
-
 
 //set initial values
 let plays = 0;
@@ -39,7 +34,7 @@ function playRound(playerSelection, computerSelection) {
 
     document.querySelector('.round-number').textContent = "Round " + plays;
 
-    //Change computer's selection image
+    //change computer's selection image
     function computerImage(computerSelection) {
       if (computerSelection == "ROCK") {
         document.querySelector('.computer-selection').src = 'rock_image.png';
@@ -50,12 +45,12 @@ function playRound(playerSelection, computerSelection) {
       }
     }
     computerImage(computerSelection);
-
+    //get results
     if (playerSelection === "ROCK" && computerSelection === "ROCK" ||
     playerSelection === "PAPER" && computerSelection === "PAPER" ||
     playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
 
-    document.querySelector('.result').textContent = "It\'s a draw!";
+    document.querySelector('.result').textContent = "It's a draw!";
 
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER" ||
     playerSelection === "PAPER" && computerSelection === "SCISSORS" ||
@@ -68,84 +63,41 @@ function playRound(playerSelection, computerSelection) {
     document.querySelector('.result').textContent = "You win! " + playerSelection + " beats " + computerSelection;
     playerScore++;
     }
+
     //display score results
     document.querySelector('.player-score').textContent =+playerScore;
     document.querySelector('.computer-score').textContent =+computerScore;
 
-  //display winner of match and reset
-  if (plays == 5) {
-    if (playerScore < computerScore) {
-      document.querySelector('.result').textContent = "Game over! You lost";
-      document.querySelector('.result').style.color = "red";
+    //display winner of match and reset
+    if (plays == 5) {
+      if (playerScore < computerScore) {
+        document.querySelector('.result').textContent = "Game over! You lost";
+        document.querySelector('.result').style.color = "red";
+      }
+      if (playerScore > computerScore) {
+        document.querySelector('.result').textContent = "Congratulations! You won!";
+        document.querySelector('.result').style.color = "green";
+      }
+      if (playerScore == computerScore) {
+        document.querySelector('.result').textContent = "It\'s a tie! Try again.";
+        document.querySelector('.result').style.color = "orange";
+      }
+
+      //remove buttons and result at the end of 5 rounds
+      //create new button to reload page
+      var elem = document.querySelector('.choices');
+      elem.parentNode.removeChild(elem);
+    
+      var elem = document.querySelector('#command');
+      elem.parentNode.removeChild(elem);
+
+      const butt = document.querySelector('.restart-button');
+      butt.textContent = 'New Match';
+      butt.setAttribute('style', 'display: inline-block;');
+
+      //reload page
+      butt.addEventListener('click', () => {
+        location.reload();
+      });
     }
-    if (playerScore > computerScore) {
-      document.querySelector('.result').textContent = "Congratulations! You won!";
-      document.querySelector('.result').style.color = "green";
-    }
-    if (playerScore == computerScore) {
-      document.querySelector('.result').textContent = "It\'s a tie! Try again.";
-      document.querySelector('.result').style.color = "orange";
-    }
-
-    //remove buttons and result notes at the end of 5 rounds
-    //create new button to reload page
-    var elem = document.querySelector('.choices');
-    elem.parentNode.removeChild(elem);
-   
-    var elem = document.querySelector('#command');
-    elem.parentNode.removeChild(elem);
-
-    const butt = document.querySelector('.restart-button');
-    butt.textContent = 'New Match';
-    butt.setAttribute('style', 'display: inline-block;');
-
-    //reload page
-    butt.addEventListener('click', () => {
-      location.reload();
-    });
-  }
-
 }
-/*
-
-const rock_div = document.getElementById("rock");
-const paper_div = document.getElementById("paper");
-const scissors_div = document.getElementById("scissors");
-
-function game(userChoice) {
-    const computerChoice = computerPlay();
-    switch (userChoice + computerChoice) {
-        case "rs":
-        case "pr":
-        case "sp":
-            console.log("youwin");
-            break;
-        case "rp":
-        case "ps":
-        case "sr":
-            lose();
-            break;
-        case "rr":
-        case "pp":
-        case "ss": 
-            draw();
-            break;
-    }
-
-}
-*/
-/*
-//what happens when you click
-rock_div.addEventListener("click", () => {
-    computerPlay();
-    playerImageChange();
-})
-paper_div.addEventListener("click", () => {
-    computerPlay();
-    playerImageChange();
-})
-scissors_div.addEventListener("click", () => {
-    computerPlay();
-    playerImageChange();
-})
-*/
